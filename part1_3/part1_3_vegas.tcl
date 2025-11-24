a5a3120a12e8a1c2acfd3b9d6897859ff4fe3676
jafarizadeh <jafarizadeh89@gmail.com>
2025-11-24 15:34:12 +0100

    Partie 1.3: RTT homogenes, etude file goulet et comparaison variantes TCP

---
+++ part1_3/part1_3_vegas.tcl
@@ -0,0 +1,88 @@
+# ================================
+# Partie 1.3 - Vegas
+# RTT homogenes + file du goulet
+# ================================
+
+set ns [new Simulator]
+
+set ftrace [open part1_3_vegas.tr w]
+$ns trace-all $ftrace
+
+# Noeuds
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+set n6 [$ns node]
+set n7 [$ns node]
+
+# Liens d'acces (RTT homogenes)
+$ns duplex-link $n0 $n3 100Mb 10ms DropTail
+$ns duplex-link $n1 $n3 100Mb 10ms DropTail
+$ns duplex-link $n2 $n3 100Mb 10ms DropTail
+
+$ns duplex-link $n4 $n5 100Mb 10ms DropTail
+$ns duplex-link $n4 $n6 100Mb 10ms DropTail
+$ns duplex-link $n4 $n7 100Mb 10ms DropTail
+
+# Goulet
+$ns duplex-link $n3 $n4 10Mb 10ms DropTail
+$ns queue-limit $n3 $n4 50
+$ns queue-limit $n4 $n3 50
+
+# Trace de la file du goulet
+set qtrace [open part1_3_vegas_queue.tr w]
+$ns trace-queue $n3 $n4 $qtrace
+
+proc create_tcp_ftp {ns src dst fid variant tcpName sinkName ftpName} {
+    upvar $tcpName tcp
+    upvar $sinkName sink
+    upvar $ftpName ftp
+
+    set tcp [new Agent/TCP/$variant]
+    $tcp set fid_ $fid
+    $tcp set window_ 2000
+    $tcp set maxcwnd_ 2000
+    $ns attach-agent $src $tcp
+
+    set sink [new Agent/TCPSink]
+    $ns attach-agent $dst $sink
+
+    $ns connect $tcp $sink
+
+    set ftp [new Application/FTP]
+    $ftp attach-agent $tcp
+}
+
+# 6 flux Vegas
+create_tcp_ftp $ns $n0 $n5 0 Vegas tcp0 sink0 ftp0
+create_tcp_ftp $ns $n0 $n5 1 Vegas tcp1 sink1 ftp1
+
+create_tcp_ftp $ns $n1 $n6 2 Vegas tcp2 sink2 ftp2
+create_tcp_ftp $ns $n1 $n6 3 Vegas tcp3 sink3 ftp3
+
+create_tcp_ftp $ns $n2 $n7 4 Vegas tcp4 sink4 ftp4
+create_tcp_ftp $ns $n2 $n7 5 Vegas tcp5 sink5 ftp5
+
+set simTime 200.0
+
+$ns at 0.5 "$ftp0 start"
+$ns at 0.5 "$ftp1 start"
+$ns at 0.5 "$ftp2 start"
+$ns at 0.5 "$ftp3 start"
+$ns at 0.5 "$ftp4 start"
+$ns at 0.5 "$ftp5 start"
+
+proc finish {} {
+    global ns ftrace qtrace simTime
+    $ns flush-trace
+    close $ftrace
+    close $qtrace
+    puts "Fin Vegas t=$simTime s"
+    exit 0
+}
+
+$ns at $simTime "finish"
+$ns run
